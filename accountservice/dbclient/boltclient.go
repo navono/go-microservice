0e8e75f9cd4c407ea06d85405b0bd9f1f936879f
navono <navono007@gmail.com>
2018-06-26 08:15:42 +0800

    fix the build error

--- accountservice/dbclient/boltclient.go
+++ accountservice/dbclient/boltclient.go
@@ -34,8 +34,38 @@ func (bc *BoltClient) OpenBoltDb() {
 
 // Seed Start seeding accounts
 func (bc *BoltClient) Seed() {
-	initializeBucket()
-	seedAccounts()
+	bc.initializeBucket()
+	bc.seedAccounts()
+}
+
+// QueryAccount ...
+func (bc *BoltClient) QueryAccount(accountID string) (model.Account, error) {
+	// Allocate an empty Account instance we'll let json.Unmarhal populate for us in a bit.
+	account := model.Account{}
+
+	// Read an object from the bucket using boltDB.View
+	err := bc.boltDB.View(func(tx *bolt.Tx) error {
+		// Read the bucket from the DB
+		b := tx.Bucket([]byte("AccountBucket"))
+
+		// Read the value identified by our accountID supplied as []byte
+		accountBytes := b.Get([]byte(accountID))
+		if accountBytes == nil {
+			return fmt.Errorf("No account found for " + accountID)
+		}
+		// Unmarshal the returned bytes into the account struct we created at
+		// the top of the function
+		json.Unmarshal(accountBytes, &account)
+
+		// Return nil to indicate nothing went wrong, e.g no error
+		return nil
+	})
+	// If there were an error, return the error
+	if err != nil {
+		return model.Account{}, err
+	}
+	// Return the Account struct and nil as error.
+	return account, nil
 }
 
 // Creates an "AccountBucket" in our BoltDB. It will overwrite any existing bucket of the same name.
@@ -58,7 +88,7 @@ func (bc *BoltClient) seedAccounts() {
 
 		// Create an instance of our Account struct
 		acc := model.Account{
-			Id:   key,
+			ID:   key,
 			Name: "Person_" + strconv.Itoa(i),
 		}
 
